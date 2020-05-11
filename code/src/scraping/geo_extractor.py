0de609dea6946d99062d89e6b3064664dc2c4962
Rodolfo De Nadai <rdenadai@gmail.com>
2020-05-11 15:11:23 -0300

    Commit to database

--- code/src/scraping/geo_extractor.py
+++ code/src/scraping/geo_extractor.py
@@ -6,6 +6,7 @@ import time
 import pandas as pd
 from peewee import JOIN
 
+from ..database.conn import db
 from ..database.models import RawHashtagComments, UserLocation
 from .utils import run_user_geo, run_save_user_location
 from ..processing.utils import normalizar, divide_chunks
@@ -35,32 +36,38 @@ if __name__ == "__main__":
     # Realizar o processo de pesquisa no twitter, extração e gravação na base de dados
     with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
         start_time_t = time.time()
-        for usernames in divide_chunks(usernames_d, 20):
-            start_time = time.time()
-            contents = list(
-                filter(None, executor.map(run_user_geo, usernames, chunksize=5))
-            )
-            print(f"--- Load geo took {round(time.time() - start_time, 2)} seconds ---")
+        with db.atomic() as txn:
+            for usernames in divide_chunks(usernames_d, 20):
+                start_time = time.time()
+                contents = list(
+                    filter(None, executor.map(run_user_geo, usernames, chunksize=5))
+                )
+                print(
+                    f"--- Load geo took {round(time.time() - start_time, 2)} seconds ---"
+                )
 
-            # Normaliza os dados
-            start_time = time.time()
-            norm_geo_users = []
-            for i, content in enumerate(contents):
-                try:
-                    geo = cidades[cidades["nome_norm"] == content[1]]
-                    lat, lng = geo[["LATITUDE", "LONGITUDE"]].values.tolist()[0]
-                    norm_geo_users += [
-                        (f"@{content[0]}", geo["nome"].values[0], f"{lat},{lng}")
-                    ]
-                except:
-                    pass
-            print(
-                f"--- Data norm took {round(time.time() - start_time, 2)} seconds ---"
-            )
+                # Normaliza os dados
+                start_time = time.time()
+                norm_geo_users = []
+                for i, content in enumerate(contents):
+                    try:
+                        geo = cidades[cidades["nome_norm"] == content[1]]
+                        lat, lng = geo[["LATITUDE", "LONGITUDE"]].values.tolist()[0]
+                        norm_geo_users += [
+                            (f"@{content[0]}", geo["nome"].values[0], f"{lat},{lng}")
+                        ]
+                    except:
+                        pass
+                print(
+                    f"--- Data norm took {round(time.time() - start_time, 2)} seconds ---"
+                )
 
-            start_time = time.time()
-            executor.map(run_save_user_location, norm_geo_users, chunksize=5)
-            print(f"--- Save geo took {round(time.time() - start_time, 2)} seconds ---")
+                start_time = time.time()
+                executor.map(run_save_user_location, norm_geo_users, chunksize=5)
+                txn.commit()
+                print(
+                    f"--- Save geo took {round(time.time() - start_time, 2)} seconds ---"
+                )
         print(
             f"--- All process took {round(time.time() - start_time_t, 2)} seconds ---"
         )
