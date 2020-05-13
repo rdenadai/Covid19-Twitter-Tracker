84f7b4f1af9054a8eb6e9e68d1124c6e299366b3
Rodolfo De Nadai <rdenadai@gmail.com>
2020-05-12 21:44:20 -0300

    Carregar termos em lote

--- code/src/scraping/extractor.py
+++ code/src/scraping/extractor.py
@@ -7,6 +7,7 @@ import time
 from decouple import config
 
 from ..database.conn import db
+from ..processing.utils import divide_chunks
 from .utils import run_hashtag, run_save_hashtag
 
 
@@ -73,20 +74,24 @@ if __name__ == "__main__":
             "tosse febre coriza",
         ]
 
+    qtd = cpu_count() * 2
     n_posts_2_extract = int(config("N_POSTS_TO_EXTRACT", default=1))
-    with ProcessPoolExecutor(max_workers=cpu_count() * 2) as executor:
-        start_time = time.time()
-        contents = list(
-            executor.map(partial(run_hashtag, n_posts_2_extract), hashtags, chunksize=1)
-        )
-        os.system("pkill chromedriver")
-        print(f"--- Load tweets took {round(time.time() - start_time, 2)}s ---")
-        start_time = time.time()
-        with db.atomic() as txn:
-            salvos = list(executor.map(run_save_hashtag, contents, chunksize=25))
-            txn.commit()
-        print(f"--- Save tweets took {round(time.time() - start_time, 2)}s ---")
-        for i, item in enumerate(salvos):
-            print(
-                f"--- # of tweets for : {item['hashtag']} => {len(contents[i]['comments'])}/{item['salvos']}"
+    with ProcessPoolExecutor(max_workers=qtd) as executor:
+        for hashtags_ in divide_chunks(hashtags, qtd):
+            start_time = time.time()
+            contents = list(
+                executor.map(
+                    partial(run_hashtag, n_posts_2_extract), hashtags_, chunksize=1
+                )
             )
+            os.system("pkill chromedriver")
+            print(f"--- Load tweets took {round(time.time() - start_time, 2)}s ---")
+            start_time = time.time()
+            with db.atomic() as txn:
+                salvos = list(executor.map(run_save_hashtag, contents, chunksize=25))
+                txn.commit()
+            print(f"--- Save tweets took {round(time.time() - start_time, 2)}s ---")
+            for i, item in enumerate(salvos):
+                print(
+                    f"--- # of tweets for : {item['hashtag']} => {len(contents[i]['comments'])}/{item['salvos']}"
+                )
