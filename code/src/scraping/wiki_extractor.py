c8dd344d20efa9fbfef3249103e83e4f5f99de06
Rodolfo De Nadai <rdenadai@gmail.com>
2020-05-19 11:17:56 -0300

    Wikipedia article extractor (for use with w2v)

---
+++ code/src/scraping/wiki_extractor.py
@@ -0,0 +1,104 @@
+import os
+import pickle
+
+import wikipediaapi
+
+from ..processing.utils import normalizar
+
+
+if __name__ == "__main__":
+
+    wiki_wiki = wikipediaapi.Wikipedia(
+        "pt", extract_format=wikipediaapi.ExtractFormat.WIKI
+    )
+
+    termos = sorted(
+        list(
+            set(
+                [
+                    "doença",
+                    "febre",
+                    "dor de cabeça",
+                    "tosse",
+                    "coriza",
+                    "covid-19",
+                    "pandemia",
+                    "epidemia",
+                    "economia",
+                    "presidente",
+                    "parlamento",
+                    "medicina",
+                    "medico",
+                    "diarreia",
+                    "virus",
+                    "influenza",
+                    "gripe",
+                    "sistema imunitário",
+                    "imunologia",
+                    "dengue",
+                    "célula",
+                    "coração",
+                    "pulmão",
+                    "rim",
+                    "remédio",
+                    "medicamento",
+                    "cloroquina",
+                    "hidroxicloroquina",
+                    "medicação antimalárica",
+                    "anvisa",
+                    "parlamento",
+                    "presidencialismo",
+                    "deputado",
+                    "ministro",
+                    "infusão",
+                    "xarope",
+                    "alquimia",
+                    "farmacologia",
+                    "carbocisteína",
+                    "náuseas",
+                    "doutor",
+                    "tontura",
+                    "palpitação",
+                    "insônia",
+                    "stresse psicológico",
+                    "insuficiência cardíaca",
+                    "demência",
+                    "hipnótico",
+                    "transtorno de ansiedade",
+                    "ansiedade",
+                    "corpo humano",
+                    "fenomenologia",
+                    "inflamação",
+                    "infecção",
+                    "toxina",
+                    "hospital",
+                    "hospital psiquiátrico",
+                    "placebo",
+                    "analgésico",
+                    "comprimido",
+                    "inalação",
+                    "via respiratória",
+                    "coronavírus",
+                    "vírus_ebola",
+                    "doença_por_vírus_ébola",
+                ]
+            )
+        )
+    )
+
+    phrases = []
+    for term in termos:
+        page_py = wiki_wiki.page(term)
+        if page_py.exists():
+            paragrafos = page_py.text.split("\n")
+            for parag in paragrafos:
+                phrases += [normalizar(frase) for frase in parag.split(".")]
+        else:
+            print(f"Impossível carregar {term}")
+
+    phrases = [phrase for phrase in phrases if len(phrase) > 10]
+
+    with open(f"{os.getcwd()}/data/wikipedia.pkl", "wb") as fh:
+        pickle.dump(phrases, fh)
+    # with open(f"{os.getcwd()}/data/wikipedia.pkl", "rb") as fh:
+    #     phrases = pickle.load(fh)
