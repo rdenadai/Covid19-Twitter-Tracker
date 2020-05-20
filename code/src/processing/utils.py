914bd89101748f7841b6686678e9a97eb95b6783
Rodolfo De Nadai <rdenadai@gmail.com>
2020-05-20 12:29:11 -0300

    Avaliações

--- code/src/processing/utils.py
+++ code/src/processing/utils.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from spacy.lang.pt import Portuguese
+import spacy
 import emoji
 
 
@@ -28,15 +28,19 @@ def is_number(s):
 @lru_cache(maxsize=256)
 def get_stopwords():
     stpwords = stopwords.words("portuguese")
-    rms = ["um", "não", "mais", "muito"]
+    rms = ["um", "não", "mais", "muito", "sem", "estou", "sou"]
     for rm in rms:
         del stpwords[stpwords.index(rm)]
     return stpwords, punctuation
 
 
+def remover_acentos(txt):
+    return normalize("NFKD", txt).encode("ASCII", "ignore").decode("ASCII")
+
+
 def tokenizer(phrase, clean=False):
     if not clean:
-        phrase = clean_up(phrase, False)
+        phrase = CleanUp(return_tokens=True).fit(phrase)
     clean_frase = []
     clfa = clean_frase.append
     for palavra in phrase:
@@ -44,41 +48,6 @@ def tokenizer(phrase, clean=False):
         # clfa(STEMMER.stem(palavra))
         clfa(palavra)
     return " ".join(clean_frase).strip()
-
-
-def clean_up(phrase, join=True):
-    STOPWORDS, PUNCT = get_stopwords()
-    # Transforma as hashtags em palavras
-    try:
-        for group in re.findall(r"#\S+\b", phrase, re.DOTALL):
-            g2 = re.sub(r"([A-Z])", r" \1", group, flags=re.MULTILINE)
-            phrase = re.sub(r"{}\b".format(group), g2, phrase, flags=re.MULTILINE)
-    except Exception:
-        pass
-    # lowercase para fazer outros pre-processamentos
-    phrase = phrase.lower()
-    # Remoção de emojis
-    phrase = emoji.get_emoji_regexp().sub(r"", phrase)
-    for stw in STOPWORDS:
-        phrase = re.sub(r"\b{}\b".format(stw), "", phrase, flags=re.MULTILINE)
-    for punct in PUNCT:
-        phrase = phrase.replace(punct, " ")
-    for o, r in RM:
-        phrase = re.sub(o, r, phrase, flags=re.MULTILINE)
-
-    # Limpeza extra
-    phrase = word_tokenize(phrase)
-    clean_frase = []
-    clfa = clean_frase.append
-    for palavra in phrase:
-        if not is_number(palavra) and len(palavra) > 2:
-            clfa(palavra)
-    clean_frase = " ".join(clean_frase) if join else clean_frase
-    return clean_frase.strip()
-
-
-def remover_acentos(txt):
-    return normalize("NFKD", txt).encode("ASCII", "ignore").decode("ASCII")
 
 
 def normalizar(phrase, sort=True):
@@ -93,26 +62,103 @@ def normalizar(phrase, sort=True):
     return phrase
 
 
+class CleanUp:
+    def __init__(
+        self,
+        remove_stopwords=False,
+        remove_emojis=True,
+        stemmer=None,
+        lemmatizer=None,
+        return_tokens=False,
+    ) -> None:
+        self.remove_stopwords = remove_stopwords
+        self.remove_emojis = remove_emojis
+        self.stemmer = stemmer
+        self.lemmatizer = lemmatizer
+        self.return_tokens = return_tokens
+        self.STOPWORDS, self.PUNCT = self.get_stopwords()
+        self.RM = [
+            (r"(http[s]*?:\/\/)+.*[\r\n]*", r""),
+            (r"@", r""),
+            (r"\n+", r" . "),
+            (r'"', r" "),
+            (r"\'", r" "),
+            (r"#", r""),
+            (r"(RT)", r""),
+            (r"[…]", " . "),
+            (r"[0-9]*", r""),
+            (r"“", r""),
+            (r"”", ""),
+            (r"([aeiouqwtyupdfghjklçzxcvbnm|!@$%&\.\[\]\(\)+-_=<>,;:])\1+", r"\1"),
+            (r"(\bñ\n)", "não"),
+            (r"(nã)", "não"),
+            (r"\s+", r" "),
+            (r"(nãoo)", "não"),
+        ]
+
+    def remover_acentos(self, phrase):
+        return normalize("NFKD", phrase).encode("ASCII", "ignore").decode("ASCII")
+
+    @lru_cache(maxsize=256)
+    def is_number(self, word):
+        try:
+            complex(word)  # for int, long, float and complex
+        except ValueError:
+            return False
+        return True
+
+    @lru_cache(maxsize=256)
+    def get_stopwords(self):
+        stpwords = stopwords.words("portuguese")
+        rms = ["um", "não", "mais", "muito", "sem", "estou", "sou"]
+        for rm in rms:
+            del stpwords[stpwords.index(rm)]
+        return stpwords, punctuation
+
+    def fit(self, phrase):
+        # Transforma as hashtags em palavras
+        try:
+            for group in re.findall(r"#\S+\b", phrase, re.DOTALL):
+                g2 = re.sub(r"([A-Z])", r" \1", group, flags=re.MULTILINE)
+                phrase = re.sub(r"{}\b".format(group), g2, phrase, flags=re.MULTILINE)
+        except Exception:
+            pass
+        # lowercase para fazer outros pre-processamentos
+        phrase = phrase.lower()
+        # Remoção de emojis
+        if self.remove_emojis:
+            phrase = emoji.get_emoji_regexp().sub(r"", phrase)
+        if self.remove_stopwords:
+            for stw in self.STOPWORDS:
+                phrase = re.sub(r"\b{}\b".format(stw), "", phrase, flags=re.MULTILINE)
+        # Remove pontuação
+        for punct in self.PUNCT:
+            phrase = phrase.replace(punct, " ")
+        # Remove strings padrão existente, como urls
+        for o, r in self.RM:
+            phrase = re.sub(o, r, phrase, flags=re.MULTILINE)
+        phrase = self.remover_acentos(phrase)
+
+        # Limpeza extra
+        phrase = word_tokenize(phrase)
+        clean_frase = []
+        clfa = clean_frase.append
+        for palavra in phrase:
+            if not self.is_number(palavra) and len(palavra) > 2:
+                if self.lemmatizer:
+                    palavra = "".join(
+                        [word.lemma_ for word in self.lemmatizer(palavra)]
+                    )
+                if self.stemmer:
+                    clfa(self.stemmer.stem(palavra))
+                else:
+                    clfa(palavra)
+        clean_frase = " ".join(clean_frase) if not self.return_tokens else clean_frase
+        return clean_frase.strip()
+
+
 # GLOBALS
-NLP = Portuguese()
-# STEMMER = nltk.stem.RSLPStemmer()
-STEMMER = nltk.stem.SnowballStemmer("portuguese")
+NLP_LEMMATIZER = spacy.load("pt")
+RSLP_STEMMER = nltk.stem.RSLPStemmer()
+SNOWBALL_STEMMER = nltk.stem.SnowballStemmer("portuguese")
 STOPWORDS, PUNCT = get_stopwords()
-RM = [
-    (r"(http[s]*?:\/\/)+.*[\r\n]*", r""),
-    (r"@", r""),
-    (r"\n+", r" . "),
-    (r'"', r" "),
-    (r"\'", r" "),
-    (r"#", r""),
-    (r"(RT)", r""),
-    (r"[…]", " . "),
-    (r"[0-9]*", r""),
-    (r"“", r""),
-    (r"”", ""),
-    (r"([aeiouqwtyupdfghjklçzxcvbnm|!@$%&\.\[\]\(\)+-_=<>,;:])\1+", r"\1"),
-    (r"(\bñ\n)", "não"),
-    (r"(nã)", "não"),
-    (r"\s+", r" "),
-    (r"(nãoo)", "não"),
-]
