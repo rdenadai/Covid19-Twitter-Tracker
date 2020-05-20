306bda57aae81541c5ef8382d4f60f1102ba778c
Rodolfo De Nadai <rdenadai@gmail.com>
2020-05-20 01:20:34 -0300

    Acertos para carregar a base de dados

--- code/src/database/conn.py
+++ code/src/database/conn.py
@@ -1,12 +1,17 @@
+import os
 from decouple import config
 
 # from peewee import SqliteDatabase
 from playhouse.pool import PooledSqliteExtDatabase  # , PooledPostgresqlExtDatabase
 
 
+db_name = config("DATABASE_PATH", default="covid19_tracker.db")
+path = os.getcwd().split("code")[0]
+path = f"{path}/code"
+
 # db = SqliteDatabase(config('DATABASE_PATH', default='covid19_tracker.db'))
 db = PooledSqliteExtDatabase(
-    config("DATABASE_PATH", default="covid19_tracker.db"),
+    f"{path}/{db_name}",
     pragmas={
         "journal_mode": "wal",  # WAL-mode.
         "cache_size": -64 * 1000,  # 64MB cache.
